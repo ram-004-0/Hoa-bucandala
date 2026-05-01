34f5eca1a566157b0e17a1e7545ffe00d881edf6
Adrian-42 <rocacorba.317851@globalcity.sti.edu.ph>
2026-05-01 13:56:58 +0800

    Unified Login

--- hoa/src/props/AuthCard.jsx
+++ hoa/src/props/AuthCard.jsx
@@ -12,11 +12,14 @@ const AuthCard = () => {
     setError("");
 
     try {
-      const response = await fetch("http://localhost:5000/api/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }),
-      });
+      const response = await fetch(
+        "https://hoa-camellabucandalav-production.up.railway.app/api/login",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email, password }),
+        },
+      );
 
       const data = await response.json();
 
